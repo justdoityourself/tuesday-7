5ee44fb0c41a520124dcba5f7b3f149c7b71e7b4
Andrew Gardner <agardner@securenettech.com>
2020-03-03 18:34:44 -0700

    Automating a google search.

--- Program.cs
+++ Program.cs
@@ -5,20 +5,27 @@ using Tuesday7.Tests;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
+using System.Threading;
+
 namespace Tuesday7
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Test test = new Test(new List<TestInstruction> { new Login() });
+            IWebDriver mDriver;
 
-            Collection collection = new Collection();
-            collection.AddTest(test);
+            mDriver = new ChromeDriver(@"c:\selenium\chrome80");
+            mDriver.Url = "https://google.com";
+            mDriver.Manage().Window.Maximize();
 
-            Runner runner = new Runner(collection);
+            var element = mDriver.FindElement(By.XPath("/html/body[@id='gsr']/div[@id='viewport']/div[@id='searchform']/form[@id='tsf']/div[2]/div[@class='A8SBwf sbfc']/div[@class='RNNXgb']/div[@class='SDkEP']/div[@class='a4bIc']/input[@class='gLFyf gsfi']"));
 
-            runner.Start();
+            element.SendKeys("Google This");
+
+            Thread.Sleep(30000); //Wait for 30 seconds.
+
+            mDriver.Close();
         }
     }
 }
