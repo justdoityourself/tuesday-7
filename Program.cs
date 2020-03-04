e32f601b2d0725eba39725090d45d51d255f5aff
Andrew Gardner <agardner@securenettech.com>
2020-03-03 19:23:27 -0700

    Hit enter button

--- Program.cs
+++ Program.cs
@@ -19,9 +19,15 @@ namespace Tuesday7
             mDriver.Url = "https://google.com";
             mDriver.Manage().Window.Maximize();
 
-            var element = mDriver.FindElement(By.XPath("/html/body[@id='gsr']/div[@id='viewport']/div[@id='searchform']/form[@id='tsf']/div[2]/div[@class='A8SBwf sbfc']/div[@class='RNNXgb']/div[@class='SDkEP']/div[@class='a4bIc']/input[@class='gLFyf gsfi']"));
+            var search_input = mDriver.FindElement(By.XPath("/html/body[@id='gsr']/div[@id='viewport']/div[@id='searchform']/form[@id='tsf']/div[2]/div[@class='A8SBwf sbfc']/div[@class='RNNXgb']/div[@class='SDkEP']/div[@class='a4bIc']/input[@class='gLFyf gsfi']"));
+            //var do_search_button = mDriver.FindElement(By.XPath("/html/body[@id='gsr']/div[@id='viewport']/div[@id='searchform']/form[@id='tsf']/div[2]/div[@class='A8SBwf']/div[@class='FPdoLc tfB0Bf']/center/input[@class='gNO89b']"));
 
-            element.SendKeys("Google This");
+            search_input.SendKeys("Google This");
+            search_input.SendKeys(Keys.Return);
+
+            Thread.Sleep(3);
+
+            //do_search_button.Click();
 
             Thread.Sleep(30000); //Wait for 30 seconds.
 
